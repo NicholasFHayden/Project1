a4d6a05275a743a3c529a4a1ced9480a26dd0aa7
AlexanderLevinQU <meadowtownn@gmail.com>
2017-11-18 15:34:19 -0500

    Changed it better to suit the data
    Still need to insert values althou you said you don't wanna use MYSQL DB so xD I will stop

--- cellLineCreation.py
+++ cellLineCreation.py
@@ -1,7 +1,7 @@
 '''
 Created on Nov 18, 2017
 
-@author: Alex
+@author: Alex Levin
 '''
 import MySQLdb
 from smtplib import prompt
@@ -12,9 +12,12 @@ def main():
     password = prompt("Enter PassWord for DB: ")
     dbName = prompt("Enter DB name: ")
     data = getData()
+    table = parseData(data)
+    createDB(user, password, dbName)
+    
     
 def getData():
-    data = open("data.json", "r")
+    data = open("data.txt", "r")
     data = json.loads(data)
     return data
 
@@ -22,17 +25,23 @@ def parseData(data):
     pass
     allData = []
     rowOfData = {}
+    i = 0
     for item in data:
-        rowOfData['Type'] = item['Type']
-        rowOfData['Source'] = item['Source']
-        rowOfData['Sequence'] = item['Sequence']
-        rowOfData['Size'] = item['Size']
-        rowOfData['Tissue'] = item['Tissue']
-        rowOfData['CellGrowthTime'] = item['CellGrowthTime']
-        rowOfData['Morphology'] = item['Morphology']
-        rowOfData['CultureConditions'] = item['CultureConditions']
-        rowOfData['AdherentVSuspended'] = item['AdherentVSuspended']
-        rowOfData['Applications'] = item['Applications']
+        rowOfData['ID'] = i ##Auto Increment an ID
+        rowOfData['size'] = item['size']
+        rowOfData['culture'] = item['culture']
+        rowOfData['seq'] = item['seq']
+        rowOfData['cell'] = item['cell']
+        rowOfData['advSus'] = item['advSus']
+        rowOfData['subtype'] = item['subtype']
+        rowOfData['time'] = item['time']
+        rowOfData['productsheets'] = item['productsheets']
+        rowOfData['application'] = item['application']
+        rowOfData['institution'] = item['institution']
+        rowOfData['pubs'] = item['pubs']
+        rowOfData['type'] = item['type']
+        rowOfData['tissue'] = item['tissue']
+        i = i + 1
         
         allData.append(rowOfData.copy())
     ##Got all data in an arary now
@@ -40,7 +49,16 @@ def parseData(data):
 
 def createDB(user, password, dbName):
     db = MySQLdb.connect("localhost", user, password, dbName)
+    cursor = MySQLdb.cursors.Cursor()
+    cursor.execute("DROP TABLE IF EXISTS alldata")
+    ## NEED DATA TYPES TO ASSIGN FOR DATA TABLES
+    cursor.execute("CREATE TABLE alldata(ID INTEGER PRIMARY KEY, type TEXT, size TEXT, culture TEXT, seq TEXT, cell TEXT, advSus TEXT, subtype TEXT, time TEXT, productsheets TEXT, application TEXT, institution TEXT, pubs TEXT, tissue TEXT) ")
     ###Need To create the DB
+    cursor.close()
+    db.commit()
+    
+def insertValues():
+    pass
     
 if __name__ == '__main__':
     main()
